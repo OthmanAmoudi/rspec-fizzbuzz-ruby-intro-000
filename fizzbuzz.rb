441a78793925c52779beca3eb46102499e7a6829
Othman AMD <red.fantastic@hotmail.com>
2017-09-19 18:07:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
+def fizzbuzz(i)
+  if ((i % 5 == 0) && (i % 3 == 0))
     return "FizzBuzz"
-  elsif int % 3 == 0
+  elsif (i % 3 == 0)
     return "Fizz"
-  elsif int % 5 == 0
+  elsif (i % 5 == 0)
     return "Buzz"
+  else
+    return nil
   end
 end
